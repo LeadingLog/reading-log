ecea4e6aa66d399ddf9fe3d6cc013b31acdc20d1
haeburney <4ckdnjssu@naver.com>
2025-04-04 00:00:40 +0900

    소셜로그인 버튼 추가

--- ReadingLogFrontend/src/view/Login.tsx
+++ ReadingLogFrontend/src/view/Login.tsx
@@ -1,3 +1,6 @@
+import KaKaoLoginBtn from "../../public/image/login/kakao_login_btn.png";
+import NaverLoginBtn from "../../public/image/login/naver_login_btn.png";
+
 export default function Login() {
   return (
     <section
@@ -5,11 +8,19 @@ export default function Login() {
       <p className="text-white text-[40px]">My Reading LOG</p>
       <div className="text-white text-[40px]">로고 자리</div>
       <article className="flex flex-col gap-[20px]">
-        <button className="bg-socialBg text-[40px] w-[500px] h-[85px] rounded-xl">
-          카카오 이미지
+        <button className="bg-socialBg text-[40px] w-[400px] h-[65px] rounded-xl">
+          <img
+            src={KaKaoLoginBtn}
+            alt={"카카오 로그인"}
+            className={"hover:brightness-90"}
+          />
         </button>
-        <button className="bg-socialBg  text-[40px] w-[500px] h-[85px] rounded-lg">
-          네이버 이미지
+        <button className="bg-socialBg  text-[40px] w-[400px] h-[65px] rounded-lg">
+          <img
+            src={NaverLoginBtn}
+            alt={"네이버 로그인"}
+            className={"hover:brightness-90"}
+          />
         </button>
       </article>
     </section>
