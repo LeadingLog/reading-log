e12a7c9fbb9ea5497aff4bb9a094f56fa970a98a
haeburney <4ckdnjssu@naver.com>
2025-04-07 23:26:06 +0900

    로그인 여부 검사하는 ProtectedRoute 컴포넌트 구현

--- ReadingLogFrontend/src/router/router.tsx
+++ ReadingLogFrontend/src/router/router.tsx
@@ -1,11 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Main from "../view/Main.tsx";
 import Error from "../view/Error.tsx";
 import Login from "../view/Login.tsx";
 import NaverCallback from "../view/social/NaverCallback.tsx";
-import KakaoCallback from "../view/social/KakaoCallback.tsx"
+import KakaoCallback from "../view/social/KakaoCallback.tsx";
+//import ProtectedRoute from "../routes/ProtectedRoute.tsx";
+//import {useUserStore} from "../store/userStore.ts";
 
-export default function router() {
+export default function Router() {
+  //const token = useUserStore((state) => state.token);
+
   return (
     <Routes>
       <Route path="*" element={<Error />} />
@@ -13,6 +17,24 @@ export default function router() {
       <Route path="/login" element={<Login />} />
       <Route path="/oauth/naver/callback" element={<NaverCallback />} />
       <Route path="/oauth/kakao/callback" element={<KakaoCallback />} />
+
+      {/* 루트 경로 접근 시 로그인 여부 체크
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Main />
+          </ProtectedRoute>
+        }
+      />
+      */}
+
+      {/* 로그인 페이지는 로그인되어있으면 메인으로 리다이렉트
+      <Route
+        path="/login"
+        element={token ? <Navigate to="/" replace /> : <Login />}
+      />
+      */}
     </Routes>
   );
 }
