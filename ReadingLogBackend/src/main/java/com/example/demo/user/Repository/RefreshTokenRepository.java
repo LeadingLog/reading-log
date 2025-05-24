104182281b243d5bd069ef24766d4d738b235be6
seoyeonDev <skylarlee1003@gmail.com>
2025-05-24 09:11:16 +0900

    이서연 : backdev 배포용

--- ReadingLogBackend/src/main/java/com/example/demo/user/Repository/RefreshTokenRepository.java
+++ ReadingLogBackend/src/main/java/com/example/demo/user/Repository/RefreshTokenRepository.java
@@ -14,4 +14,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Inte
     void deleteByUserIdAndProvider(Integer userId, String provider);
     RefreshToken findByUserId(Integer userId);
 
+
+
 }
