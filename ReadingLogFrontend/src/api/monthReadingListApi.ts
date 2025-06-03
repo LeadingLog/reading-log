6849ce4c749acd134f204f08f486f3fca76b00dd
inging-bo <thervv@kakao.com>
2025-06-03 22:38:53 +0900

    이번달 독서 리스트 api 특정 년&월 독서 리스트 요청 api 구분

---
+++ ReadingLogFrontend/src/api/monthReadingListApi.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { fetchMonthReadingListParams } from "../types/monthReadingList.ts";
+
+const serverUrl = import.meta.env.VITE_SERVER_URL;
+
+export const fetchMonthReadingList = async ({
+                                                  userId,
+                                                  year,
+                                                  month,
+                                                  page,
+                                                  size,
+                                                }: fetchMonthReadingListParams) => {
+
+  const { data } = await axios.get(
+    `${serverUrl}/readinglist/yymm`,
+    {
+      params: {
+        userId: userId, // 실제 로그인 유저 ID로 교체 필요
+        year: year,
+        month: month,
+        page: page,
+        size: size
+      },
+    }
+  );
+
+  return data; // API 응답 반환
+};
