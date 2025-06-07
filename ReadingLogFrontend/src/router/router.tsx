4cc9f6695c41da286d00159f73d7f6e4730251ff
haeburney <4ckdnjssu@naver.com>
2025-06-08 00:07:33 +0900

    로그인 여부 검사 테스트7

--- ReadingLogFrontend/src/router/router.tsx
+++ ReadingLogFrontend/src/router/router.tsx
@@ -1,21 +1,20 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Main from "../view/Main.tsx";
 import Error from "../view/Error.tsx";
 import Login from "../view/Login.tsx";
 import NaverCallback from "../view/social/NaverCallback.tsx";
 import KakaoCallback from "../view/social/KakaoCallback.tsx";
 //import ProtectedRoute from "../routes/ProtectedRoute.tsx";
-import { useUserStore } from "../store/userStore.ts";
 import ProtectedRoute from "../routes/ProtectedRoute.tsx";
 
 export default function Router() {
-  const { userId } = useUserStore();
+  // const { userId } = useUserStore();
 
   return (
     <Routes>
       <Route path="*" element={<Error/>}/>
       {/*<Route path="/" element={<Main/>}/>*/}
-      {/*<Route path="/login" element={<Login/>}/>*/}
+      <Route path="/login" element={<Login/>}/>
       <Route path="/oauth/naver/callback" element={<NaverCallback/>}/>
       <Route path="/oauth/kakao/callback" element={<KakaoCallback/>}/>
 
@@ -30,10 +29,10 @@ export default function Router() {
       />
 
       {/*로그인 페이지는 로그인되어있으면 메인으로 리다이렉트*/}
-      <Route
-        path="/login"
-        element={(userId !== 0) ? <Navigate to="/" replace/> : <Login/>}
-      />
+      {/*<Route*/}
+      {/*  path="/login"*/}
+      {/*  element={(userId !== 0) ? <Navigate to="/" replace/> : <Login/>}*/}
+      {/*/>*/}
     </Routes>
   );
 }
