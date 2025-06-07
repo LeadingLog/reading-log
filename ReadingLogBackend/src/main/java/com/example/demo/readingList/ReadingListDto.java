73deedad59a3b768990aef7f3772b124ba4c3652
seoyeonDev <skylarlee1003@gmail.com>
2025-06-07 10:11:16 +0900

    이서연 : enum 추가

--- ReadingLogBackend/src/main/java/com/example/demo/readingList/ReadingListDto.java
+++ ReadingLogBackend/src/main/java/com/example/demo/readingList/ReadingListDto.java
@@ -14,31 +14,33 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class ReadingListDto {
-    private String title;
+    private String bookTitle;
     private String author;
     private String isbn13;
     private long bookId;
-    private String cover;
+    private String coverImgUrl;
+    private String link;
 
     private BookStatus bookStatus;
-    private boolean isFavorite;
-    private LocalDate createdAt;
+//    private boolean isFavorite;
+//    private LocalDate createdAt;
 
     public static ReadingListDto fromEntity(ReadingList readingList) {
         boolean isFavoriteValue = readingList.getReadEndDt()== null
                                     && readingList.getReadStartDt() == null;
 
         return ReadingListDto.builder()
-                .title(readingList.getBookTitle()) // bookTitle -> title 매핑
+                .bookTitle(readingList.getBookTitle()) // bookTitle -> title 매핑
                 .author(readingList.getAuthor())
                 .isbn13(readingList.getIsbn13())
                 .bookId(readingList.getBookId())
-                .cover(readingList.getCoverImgUrl()) // coverImgUrl -> cover 매핑
+                .coverImgUrl(readingList.getCoverImgUrl()) // coverImgUrl -> cover 매핑
+                .link(readingList.getLink())
                 .bookStatus(readingList.getBookStatus()) // 변환된 readStatus 설정
-                .isFavorite(isFavoriteValue) // 계산된 isFavorite 설정
-                .createdAt(readingList.getCreatedAt()) // createdAt 필드 설정
+//                .isFavorite(isFavoriteValue) // 계산된 isFavorite 설정
+//                .createdAt(readingList.getCreatedAt()) // createdAt 필드 설정
                 .build();
     }
-
+    
 
 }
